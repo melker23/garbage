313c80bed53bc124d389c8b0f6292675fac1b6df
melker <82175353+melker23@users.noreply.github.com>
2024-01-18 20:58:38 +0100

    Create garbage.py

---
+++ garbage.py
@@ -0,0 +1,16 @@
+
+def generate_garbage(n):
+    with open("garbage_output.py", "w") as file:
+        for num in range(1, n + 1):
+            file.write(f"if num == {num}:\n")
+            if num % 2 == 1:
+                file.write(f"    print('odd')\n")
+            elif num % 2 == 0:
+                file.write(f"    print('even')\n")
+
+def main():
+    n = int(input("Enter the value of n: "))
+    generate_garbage(n)
+
+if __name__ == "__main__":
+    main()
